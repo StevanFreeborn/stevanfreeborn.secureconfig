2babe2ce681c6b0a9bec02eddb49e7eb8327a60e
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2026-04-01 14:46:05 -0500

    refactor: place log messages in same file

--- src/StevanFreeborn.Extensions.Configuration.Secure/Cryptography/MachineIdKeyGenerator.cs
+++ src/StevanFreeborn.Extensions.Configuration.Secure/Cryptography/MachineIdKeyGenerator.cs
@@ -90,20 +90,3 @@ internal sealed class MachineIdKeyGenerator : IMachineIdKeyGenerator
     return $"{Environment.MachineName}_{Environment.UserName}";
   }
 }
-
-internal static partial class LogMessages
-{
-  [LoggerMessage(
-      EventId = 1,
-      Level = LogLevel.Warning,
-      Message = "Failed to retrieve hardware-specific machine ID. Falling back to environment variables."
-  )]
-  public static partial void LogFailedRetrievingMachineId(this ILogger logger, Exception ex);
-
-  [LoggerMessage(
-      EventId = 2,
-      Level = LogLevel.Information,
-      Message = "Using fallback strategy for Machine ID generation."
-  )]
-  public static partial void LogUsingFallbackStrategy(this ILogger logger);
-}
---
+++ src/StevanFreeborn.Extensions.Configuration.Secure/Logging/LogMessages.cs
@@ -0,0 +1,25 @@
+using Microsoft.Extensions.Logging;
+
+internal static partial class LogMessages
+{
+  [LoggerMessage(
+      EventId = 1,
+      Level = LogLevel.Warning,
+      Message = "Failed to retrieve hardware-specific machine ID. Falling back to environment variables."
+  )]
+  public static partial void LogFailedRetrievingMachineId(this ILogger logger, Exception ex);
+
+  [LoggerMessage(
+      EventId = 2,
+      Level = LogLevel.Information,
+      Message = "Using fallback strategy for Machine ID generation."
+  )]
+  public static partial void LogUsingFallbackStrategy(this ILogger logger);
+
+  [LoggerMessage(
+      EventId = 3,
+      Level = LogLevel.Error,
+      Message = "Failed to decrypt value for key {Key}"
+  )]
+  public static partial void LogDecryptionFailure(this ILogger logger, Exception ex, string key);
+}
